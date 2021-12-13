74777446f5c7455c39db049c6e4a1801871963f1
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-12-12 18:42:01 -0600

    Pizzeria1

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -33,8 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #myapps
     'pizzas',
-    'users',
-    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,8 +1,2 @@
-from django.shortcuts import render
 
-# Create your views here.
-
-def index(request):
-    """The home page for McCarty Pizzeria"""
-    return render(request, 'pizzas/index.html')
     
